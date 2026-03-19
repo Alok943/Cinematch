6dbc8012252e262829bfb261ae5134995863627b
Alok Singh <aloksingh98541@gmail.com>
2026-03-19 16:18:42 +0530

    deleted unnessecary files

--- src/test_retrieval.py
+++
@@ -1,6 +0,0 @@
-
-import chromadb
-client = chromadb.PersistentClient(path='chroma_db')
-col = client.get_collection('cine_match_v1')
-result = col.get(limit=1, include=['metadatas'])
-print(result['metadatas'][0].keys())
--- temp_check2.py
+++
@@ -1,6 +0,0 @@
-import chromadb 
-client = chromadb.PersistentClient(path='chroma_db') 
-col = client.get_collection('cine_match_v1') 
-result = col.get(where={'original_language': {'$eq': 'hi'}}, limit=5, include=['metadatas']) 
-for m in result['metadatas']: 
-    print(m['title'], m['original_language']) 
